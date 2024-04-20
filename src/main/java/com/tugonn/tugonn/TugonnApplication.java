75cfffb48538adcf08e2a6467548fbc699002409
ChhaviPriyaTanwar <chhavipriyasing@gmail.com>
2024-04-20 08:18:07 +0530

    first commit

---
+++ src/main/java/com/tugonn/tugonn/TugonnApplication.java
@@ -0,0 +1,13 @@
+package com.tugonn.tugonn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugonnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugonnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tugonn/tugonn/TugonnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tugonn.tugonn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugonnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
