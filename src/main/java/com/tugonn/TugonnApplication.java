66553dde8d26fdbb6e3a34f3646fd90e0f2c8121
ChhaviPriyaTanwar <chhavipriyasing@gmail.com>
2024-04-23 07:09:27 +0530

    code modified

--- src/main/java/com/tugonn/TugonnApplication.java
+++ src/main/java/com/tugonn/TugonnApplication.java
@@ -1,4 +1,4 @@
-package com.tugonn.tugonn;
+package com.tugonn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/tugonn/TugonnApplicationTests.java
+++ src/test/java/com/tugonn/TugonnApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tugonn.tugonn;
+package com.tugonn;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
